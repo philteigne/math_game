084b0474dafdd2e69bf478dd3e8f944f142ae669
Phil <philippe.teigne@gmail.com>
2024-03-25 17:57:11 -0400

    implement prompt_question and adjust player lives based on user input

--- main.rb
+++ main.rb
@@ -4,7 +4,13 @@ require "./classes/player.rb"
 Player1 = Player.new()
 Player2 = Player.new()
 
-question_count = 0
+# prompt user with question and check answer
+# return true if answer given is correct
+def prompt_question(player, question)
+  puts "----- NEW TURN -----"
+  puts "#{player}: What does #{question.num1} plus #{question.num2} equal?"
+  return gets.chomp() == question.answer
+end
 
 # If either player has lives the game continues
 while Player1.lives > 0 && Player2.lives > 0
@@ -16,6 +22,22 @@ while Player1.lives > 0 && Player2.lives > 0
     current_turn = "Player 1"
   else
     current_turn = "Player 2"
+  end
+
+  # Prompt current player with question and await input
+  current_question = Question.new()
+
+  if prompt_question(current_turn, current_question)
+    puts "YES! You are correct."
+  else
+    if current_turn == "Player 1"
+      Player1.lives -= 1
+    end
+    if current_turn == "Player 2"
+      Player2.lives -= 1
+    end
+
+    puts "Seriously? No!"
   end
 
 end
