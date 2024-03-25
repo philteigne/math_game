564e2e14526e705ffdf9e8d2585b34dfb2cf5b57
Phil <philippe.teigne@gmail.com>
2024-03-25 17:30:56 -0400

    start a game and determine whose turn it is

--- main.rb
+++ main.rb
@@ -3,3 +3,19 @@ require "./classes/player.rb"
 
 Player1 = Player.new()
 Player2 = Player.new()
+
+question_count = 0
+
+# If either player has lives the game continues
+while Player1.lives > 0 && Player2.lives > 0
+
+  question_count += 1
+
+  # Set current player turn
+  if question_count % 2 != 0
+    current_turn = "Player 1"
+  else
+    current_turn = "Player 2"
+  end
+
+end
